c1f4b830425926d958af18bca905e86979184947
Mansir <mansiraziz93@gmail.com>
2023-09-20 12:22:35 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    // "./node_modules/tw-elements-react/dist/js/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  // plugins: [require("tw-elements-react/dist/plugin.cjs")]
 });

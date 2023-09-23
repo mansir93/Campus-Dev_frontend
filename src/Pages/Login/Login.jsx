2e3c752176e7e877a6f01c0b498171871a1241db
Mansir <mansiraziz93@gmail.com>
2023-09-23 17:07:40 +0000

    Update Login.jsx

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -144,7 +144,7 @@ const Login = () => {
                 </Card>
               </div>
 
-              <div className="lg:w-1/2 max-h-96  flex flex-col justify-center p-4 leading-normal bg-blue-500">
+              <div className="lg:w-1/2 flex flex-col justify-center p-4 leading-normal bg-blue-500">
                 {/* <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"></h5> */}
                 {/* <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 Access a suite of powerful
